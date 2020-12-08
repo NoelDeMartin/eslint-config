9c0df7008114a145b09bfe36ca6ecf593249cd34
Noel De Martin <noeldemartin@gmail.com>
2020-12-08 18:24:03 +0100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# ESLint TypeScript config
+
+These are the TypeScript ESLint rules I use in different projects. If you want to use them as well, you're most welcome. You can install the package with:
+
+```sh
+npm install eslint @noeldemartin/eslint-config-typescript --save-dev
+```
+
+And use it in your eslint config file:
+
+```js
+// .eslintrc.js
+module.exports = {
+    extends: ['@noeldemartin/eslint-config-typescript'],
+};
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+        '@typescript-eslint/no-explicit-any': [
+            'warn',
+            {
+                ignoreRestArgs: true,
+            },
+        ],
+        '@typescript-eslint/no-inferrable-types': 'off',
+        'comma-dangle': [
+            'error',
+            'always-multiline',
+        ],
+        'indent': [
+            'error',
+            4,
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        'max-len': [
+            'error',
+            120,
+        ],
+        'quotes': [
+            'error',
+            'single',
+        ],
+        'quote-props': [
+            'error',
+            'consistent-as-needed',
+        ],
+        'semi': [
+            'error',
+            'always',
+        ],
+    },
+};
