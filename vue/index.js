5bc150e993258e0e4b7e573aa0a825415322b4ee
Noel De Martin <noeldemartin@gmail.com>
2021-03-06 17:55:33 +0100

    Add Vue rules

---
+++ vue/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: [
+        '@noeldemartin/eslint-config-typescript',
+        'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+        'vue/html-indent': ['error', 4],
+        'vue/max-attributes-per-line': ['error', { singleline: 3 }],
+        'vue/max-len': ['error', {
+            code: 120,
+            ignoreHTMLAttributeValues: true,
+        }],
+        'max-len': 'off',
+    },
+};
