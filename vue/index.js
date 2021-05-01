9814af6f5eb0b490ad6123462e5f070dec0648b9
Noel De Martin <noeldemartin@gmail.com>
2021-05-01 13:04:07 +0200

    Limit vue rules to *.vue files

--- vue/index.js
+++ vue/index.js
@@ -1,15 +1,18 @@
 module.exports = {
-    extends: [
-        '@noeldemartin/eslint-config-typescript',
-        'plugin:vue/vue3-recommended',
+    extends: ['@noeldemartin/eslint-config-typescript'],
+    overrides: [
+        {
+            files: ['*.vue'],
+            extends: ['plugin:vue/vue3-recommended'],
+            rules: {
+                'vue/html-indent': ['error', 4],
+                'vue/max-attributes-per-line': ['error', { singleline: 3 }],
+                'vue/max-len': ['error', {
+                    code: 120,
+                    ignoreHTMLAttributeValues: true,
+                }],
+                'max-len': 'off',
+            },
+        },
     ],
-    rules: {
-        'vue/html-indent': ['error', 4],
-        'vue/max-attributes-per-line': ['error', { singleline: 3 }],
-        'vue/max-len': ['error', {
-            code: 120,
-            ignoreHTMLAttributeValues: true,
-        }],
-        'max-len': 'off',
-    },
 };
